f9a0030b0ddd2f63b7fee57909c43309d36d39cb
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2016-08-18 09:28:34 +0900

    enable Stetho

--- app/src/main/java/com/kamedon/androidlibssamples/AndroidLibsSamplesApplication.kt
+++ app/src/main/java/com/kamedon/androidlibssamples/AndroidLibsSamplesApplication.kt
@@ -1,15 +1,17 @@
 package com.kamedon.androidlibssamples
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.squareup.leakcanary.LeakCanary
 
 /**
  * Created by kamei.hidetoshi on 2016/08/18.
  */
-class AndroidLibsSamplesApplication : Application(){
+class AndroidLibsSamplesApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         LeakCanary.install(this)
+        Stetho.initializeWithDefaults(this)
     }
 
 }
