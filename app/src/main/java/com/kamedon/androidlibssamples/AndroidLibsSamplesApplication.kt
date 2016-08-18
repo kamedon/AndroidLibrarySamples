d8681bbfef0a788668c9591b0b565131c00332b1
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2016-08-18 09:56:10 +0900

    configure Timber

--- app/src/main/java/com/kamedon/androidlibssamples/AndroidLibsSamplesApplication.kt
+++ app/src/main/java/com/kamedon/androidlibssamples/AndroidLibsSamplesApplication.kt
@@ -3,6 +3,7 @@ package com.kamedon.androidlibssamples
 import android.app.Application
 import com.facebook.stetho.Stetho
 import com.squareup.leakcanary.LeakCanary
+import timber.log.Timber
 
 /**
  * Created by kamei.hidetoshi on 2016/08/18.
@@ -12,6 +13,10 @@ class AndroidLibsSamplesApplication : Application() {
         super.onCreate()
         LeakCanary.install(this)
         Stetho.initializeWithDefaults(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
     }
 
 }
